e83f563d9411e72539c122c135c7fb141d3b6f1d
xananax <xananax@yelostudio.com>
2015-04-13 07:17:45 +0300

    fabulous

---
+++ main.js
@@ -0,0 +1,22 @@
+$(function() {
+    $('li > ul').each(function(i) {
+        var $ul = $(this);
+        var $a = $ul.siblings('a');
+        var $parent = $ul.parent();
+        $parent.addClass('folder');
+        var $i = $('<i/>');
+        $a.prepend($i);
+        $i.on('click',function(e){
+            e.preventDefault()
+            $parent.toggleClass('open')
+            $ul.toggleClass('open');
+        });
+    }).find('a').each(function(){
+        var $a = $(this);
+        if($a.hasClass('active')){
+            $a.parents('li,ul').addClass('open');
+            $a.siblings('ul').addClass('open');
+            return false;
+        }
+    });
+});
